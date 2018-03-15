4265b8ebedc0a436bb85f63b023e19d8976e16d5
cooksey3 <chris@cookseys.org>
2018-03-15 10:12:24 -0400

    First commit

---
+++ src/main/java/org/wecancodeit/columbus/citiesfishes/CitiesFishesApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.columbus.citiesfishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesFishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesFishesApplication.class, args);
+	}
+}
